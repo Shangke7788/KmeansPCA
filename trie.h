b3cab8ebaa39c81feac7cf620be2d14ee41c0d8f
Shangke7788 <jsczxb1992@163.com>
2014-05-10 01:09:28 +0800

    Write the definition of the Trie class functions

--- trie.h
+++ trie.h
@@ -1,11 +1,67 @@
 #ifndef _SHANGKE_TRIE_H_
 #define _SHANGKE_TRIE_H_
 
-#include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 class Trie {
+	private:
+		const static int CHAR_SET_NUM = 26;
+		int cnt;
+		int tot_cnt;
+		Trie *next[CHAR_SET_NUM];
+	public:
+		/* *
+		 * 构造器
+		 */
+		Trie();
+		Trie(const Trie& o);
+
+		/* *
+		 * 析构函数
+		 */
+		~Trie();
+
+		/* *
+		 * 运算符重载
+		 */
+		// 合并两颗Trie树，返回新节点
+		Trie operator + (const Trie& o);
+
+		// 简单的复制，不增加新的空间
+		Trie &operator = (const Trie& o);
+
+		// 合并在原树上
+		Trie &operator += (const Trie& o);
+
+		/* *
+		 * 一般函数
+		 */
+		// 整体拷贝，创建新节点
+		Trie* copy() const;
+
+		// 获取该单词的数目
+		int get_cnt() const;
+
+		// 获取该前缀的数目
+		int get_tot_cnt() const;
+
+		// 插入一个单词
+		void insert(char* s);
+
+		// 查询某个单词的数目
+		int query(char* s) const;
+
+		// 获取某个前缀的数目
+		int query_pre(char* s) const;
+
+		// 删除某个单词
+		void remove(char* s);
+
+		// 获取所有的单词及其数目
+		vector< pair<int, string> > get_all() const;
 };
 
 #endif
