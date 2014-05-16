a23a2f29e73ea0b1939b46d0d16abe817109874d
Shangke7788 <jsczxb1992@163.com>
2014-05-16 15:12:51 +0800

    Set main.cpp

--- compile.bat
+++ compile.bat
@@ -3,5 +3,5 @@ g++ -c -O2 -fwrapv -o datapoint.o datapoint.cpp
 g++ -c -O2 -fwrapv -o kmeans.o kmeans.cpp
 g++ -c -O2 -fwrapv -o trie.o trie.cpp
 g++ -c -O2 -fwrapv -o tfidf.o tfidf.cpp
-g++ -O2 -fwrapv -o test.exe test.cpp kmeans.o datapoint.o matrix.o tfidf.o trie.o
+g++ -O2 -fwrapv -o main.exe main.cpp kmeans.o datapoint.o matrix.o tfidf.o trie.o
 del *.o
